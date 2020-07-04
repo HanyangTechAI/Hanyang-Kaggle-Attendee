eee391e4d2065c5d56d12829a319de5cfebceefd
Youngchae James Chee <litcoderr@gmail.com>
2020-07-04 23:22:26 +0900

    week4: fix app.py

--- week4/app.py
+++ week4/app.py
@@ -5,22 +5,26 @@ class SubmittedApp:
     def __init__(self):
         pass
 
-    def run(input_tensor: torch.Tensor) -> torch.Tensor:
+    def run(self, input_tensor: torch.Tensor) -> torch.Tensor:
         """Main Run Method for scoring system
-        :param input_tensor: (torch.Tensor)
-            [batchsize, channel(3), width, height]
-        :return: (torch.Tensor)
-            [batchsize, channel(3), width, height]
+        :param input_tensor: (torch.Tensor) [batchsize, image_dim(784)]
+        :return: (torch.Tensor) [batchsize, n_classes(10)]
         """
         return input_tensor
 
-    def metric(self, inferred_tensor: torch.Tensor, ground_truth: torch.Tensor) -> torch.Tensor:
-        """Metric Method for students to test on
-        :param inferred_tensor: (torch.Tensor)
-            문제 출제자들은 inferred_tensor shape 을 작성해줄것
-        :param ground_truth:  (torch.Tensor)
-            문제 출제자들은 output_tensor shape 을 작성해줄것
+    @staticmethod
+    def metric(inferred_tensor: torch.Tensor, ground_truth: torch.Tensor) -> torch.Tensor:
+        """Classification Accuracy
+        example)
+        inferred_tensor: [[0,0,1,0,0,0,0,0,0,0], [0,0,0,0,0,0,1,0,0,0]]
+        ground_truth: [2, 5]
+        return: 0.5
+
+        :param inferred_tensor: (torch.Tensor) [batch_size, n_classes(10)], inferred logits
+        :param ground_truth:  (torch.LongTensor) [batch_size], ground truth labels
+                                each consisting LongTensor ranging from 0 to 9
         :return: (torch.Tensor) metric 점수
         """
-        # 출제자들은 메트릭을 작성해주시기 바랍니다
-        raise NotImplementedError
+        inferred_tensor = torch.argmax(inferred_tensor, dim=-1)
+        acc = torch.mean((inferred_tensor == ground_truth).to(torch.float), dim=-1)
+        return acc
